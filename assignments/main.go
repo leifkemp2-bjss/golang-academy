a59ddc62fd104ce7db910d8e2e9af43b9aab465a
Leif Kemp <leif.kemp2@bjss.com>
2024-09-03 11:05:09 +0100

    remove no longer used lines

--- assignments/main.go
+++ assignments/main.go
@@ -19,18 +19,6 @@ func main() {
 		"5": assignment5,
 		"6": assignment6,
 	}
-	// pl("Assignment 1")
-	// assignment1()
-	// pl("\nAssignment 2")
-	// assignment2()
-	// pl("\nAssignment 3")
-	// assignment3()
-	// pl("\nAssignment 4")
-	// assignment4()
-	// pl("\nAssignment 5")
-	// assignment5()
-	// pl("\nAssignment 6")
-	// assignment6()
 
 	pl("Select an assignment (1-6)")
 
